28d1dc8b8c1a1884fb1f0f1672f2489fbc9b9700
nirgluzman <nir.gluzman@gmail.com>
2023-03-16 00:23:27 +0100

    useRef to store the previous value of a State

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [name, setName] = useState("");
+
+  const prevName = useRef(""); // useRef to store the previous value of name State
   const inputRef = useRef();
   const renderCount = useRef(0);
 
   useEffect(() => {
     renderCount.current = renderCount.current + 1;
   });
+
+  useEffect(() => {
+    prevName.current = name;
+  }, [name]);
 
   function focus() {
     console.log(inputRef.current);
@@ -21,7 +27,9 @@ function App() {
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
-      <div>My name is: {name}</div>
+      <div>
+        My name is {name} and it used to be {prevName.current}
+      </div>
       <div>I rendered {renderCount.current} times</div>
 
       <button onClick={focus}>Focus</button>
