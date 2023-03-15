8353af7b703ab141802e78018448fda5cd4e84b8
nirgluzman <nir.gluzman@gmail.com>
2023-03-16 00:18:58 +0100

    add useRef to reference element inside HTML

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [name, setName] = useState("");
+  const inputRef = useRef();
   const renderCount = useRef(0);
 
   useEffect(() => {
     renderCount.current = renderCount.current + 1;
   });
 
+  function focus() {
+    console.log(inputRef.current);
+    inputRef.current.focus();
+  }
+
   return (
     <>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <input
+        ref={inputRef} // reference element inside HTML
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <div>My name is: {name}</div>
       <div>I rendered {renderCount.current} times</div>
+
+      <button onClick={focus}>Focus</button>
     </>
   );
 }
