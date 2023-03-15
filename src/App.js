650aa06dbcf09e624803d7f832cdc90be9dd61b6
nirgluzman <nir.gluzman@gmail.com>
2023-03-16 00:11:14 +0100

    render count with useRef

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [name, setName] = useState("");
+  const renderCount = useRef(0);
+
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+  });
 
   return (
     <>
       <input value={name} onChange={(e) => setName(e.target.value)} />
       <div>My name is: {name}</div>
+      <div>I rendered {renderCount.current} times</div>
     </>
   );
 }
